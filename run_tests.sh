bf0d46f00cd4359e85ac10c840f09b224fe7d585
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-02-18 19:02:50 +0100

    Add coverage reporting

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-PYTHONPATH=./src python -m unittest discover -s tests -p "tst_*.py"
+PYTHONPATH=./src python3 -m coverage run -m unittest discover -s tests -p "tst_*.py"
+python3 -m coverage xml -o cobertura.xml
