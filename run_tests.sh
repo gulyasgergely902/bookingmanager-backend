cfad53c9bb77281dceada9c8099ccaa5be16360b
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-02-19 11:40:16 +0100

    Modify test run code

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-PYTHONPATH=./src python3 -m coverage run -m unittest discover -s tests -p "tst_*.py"
-python3 -m coverage xml -o cobertura.xml
+EXPORT=0
+HTML=0
+
+while getopts "eh" opt; do
+    case "${opt}" in
+        e)
+            EXPORT=1
+            ;;
+        h)
+            HTML=1
+            ;;
+        *)
+            echo "Invalid option: -${OPTARG}" >&2
+            exit 1
+            ;;
+    esac
+done
+
+
+PYTHONPATH=./src python3 -m coverage run --omit="tests/*" -m unittest discover -s tests -p "tst_*.py"
+python3 -m coverage report -m
+if [ $EXPORT -eq 1 ]; then
+    echo "Exporting coverage for codacy"
+    python3 -m coverage xml -o cobertura.xml
+fi
+if [ $HTML -eq 1 ]; then
+    echo "Exporting coverage for html"
+    python3 -m coverage html
+fi
